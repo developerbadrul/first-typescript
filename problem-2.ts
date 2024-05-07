288baac0c88202566d28e8dde2f13f52316c7f07
--delete <--delete>
2024-05-08 01:58:50 +0600

    solve problem 2

---
+++ problem-2.ts
@@ -0,0 +1,12 @@
+{
+    //
+    const findLargestNumber = (value: Array<number>) : number => {
+        return Math.max(...value)
+
+    }
+
+    const result = findLargestNumber([10, 5, 8, 20, 3])
+    console.log(result);
+
+    //
+}
